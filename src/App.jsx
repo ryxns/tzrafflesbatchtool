a5d8d3dc1b8ec11b57701d4285a32a42ad0de332
ryxns <33070164+ryxns@users.noreply.github.com>
2022-07-09 22:32:53 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
       <div className="container pt-10 mx-auto">
         <div className="flex justify-center">
           <h1 className="text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-red-500 via-blue-300 to-red-400">
-            NFTBakery Batch Buy Tool
+            TzRaffles Batch Buy Tool
           </h1>
         </div>
         <div className="mt-20 flex justify-center">
