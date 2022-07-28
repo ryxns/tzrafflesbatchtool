96999c87d7589efc48facc286340295adf806a38
ryxns <33070164+ryxns@users.noreply.github.com>
2022-07-28 13:26:06 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,8 @@ const App = () => {
         <div class="absolute top-10 right-10">
           <Navbar />
         </div>
-        <div class="absolute inset-x-0 bottom-14">
+        <div class="absolute inset-x-0 bottom-12">
           <ChangeQuantity />
-        </div>
-        <div class="absolute inset-x-0 bottom-0">
-          made by ryxns
         </div>
       </div> 
     </div>
