508fa46b6f542608b3576cae4a1a150ccf3b54db
ryxns <33070164+ryxns@users.noreply.github.com>
2022-07-27 22:37:38 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div>
       <div>
-        <div class="bg-purp absolute top-0 left-0 min-h-screen">
+        <div class="bg-black absolute top-0 left-0 min-h-screen">
           <img alt="" src='https://s3.us-west-2.amazonaws.com/henftz.xyz/tzraffles/tzraffle-bg.png' />
         </div>      
         <div class="absolute top-10 right-10">
