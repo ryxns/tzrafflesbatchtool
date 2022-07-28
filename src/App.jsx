13a506bb19a9054f6703b474567ecd021b9d0998
ryxns <33070164+ryxns@users.noreply.github.com>
2022-07-27 22:08:42 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,12 @@ const App = () => {
       <div>
         <div class="bg-purp absolute top-0 left-0 min-h-screen">
           <img alt="" src='https://s3.us-west-2.amazonaws.com/henftz.xyz/tzraffles/tzraffle-bg.png' />
-          <br />
-          <ChangeQuantity />
         </div>      
         <div class="absolute top-10 right-10">
           <Navbar />
+        </div>
+        <div class="absolute inset-x-0 bottom-0">
+          <ChangeQuantity />
         </div>
       </div> 
     </div>
