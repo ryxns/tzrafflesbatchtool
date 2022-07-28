e29445348d00455ca2451046a07c83b182f306a3
ryxns <33070164+ryxns@users.noreply.github.com>
2022-07-27 20:13:43 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import ChangeQuantity from "./components/ChangeName";
 const App = () => {
   return (
     <div>
-      <img src="https://s3.us-west-2.amazonaws.com/henftz.xyz/tzraffles/banner.jpg" />
       <Navbar />
+      <img src="https://s3.us-west-2.amazonaws.com/henftz.xyz/tzraffles/banner.jpg" />
       <div className="container pt-10 mx-auto bg-[url('./components/banner.jpg')]">
         <div className="flex justify-center bg-[url('./components/banner.jpg')]">
           <h1 className="text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-red-500 via-blue-300 to-red-400">
