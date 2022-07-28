d2f77c7bdfda873885c801685f54b8680b921153
ryxns <33070164+ryxns@users.noreply.github.com>
2022-07-27 21:05:11 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,15 @@ import ChangeQuantity from "./components/ChangeName";
 const App = () => {
   return (
     <div>
-    <div>
-      <div style="z-index: 1; position: absolute; top: 0; left: 0; background-image:url(https://s3.us-west-2.amazonaws.com/henftz.xyz/tzraffles/banner.jpg);background-position:100px 200px; height:250px; width:500px;">&nbsp;
-      </div>
+      <div>
+        <div style="z-index: 1; position: absolute; top: 0; left: 0;">&nbsp;
+          <img alt="" src='https://s3.us-west-2.amazonaws.com/henftz.xyz/tzraffles/banner.jpg' />
+        </div>
       
-      <div style="z-index: 1; position: absolute; top: 10; right: 10;">
-        <Navbar />
-      </div>
-    </div>  
+        <div style="z-index: 1; position: absolute; top: 10; right: 10;">
+          <Navbar />
+        </div>
+      </div>  
       <div className="container pt-10 mx-auto">
         <div className="flex justify-center">
           <h1 className="text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-red-500 via-blue-300 to-red-400">
@@ -21,9 +22,7 @@ const App = () => {
         <div className="mt-20 flex justify-center">
           <ChangeQuantity />
         </div>
-
       </div>
-    
     </div>
   );
 };
