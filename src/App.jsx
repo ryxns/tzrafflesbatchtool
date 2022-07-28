1480e412ab2eadeff82e92f9aeaf9add8343f67d
ryxns <33070164+ryxns@users.noreply.github.com>
2022-07-27 21:36:56 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import ChangeQuantity from "./components/ChangeName";
 
 const App = () => {
   return (
-    <div class="bg-blue-500 min-h-screen">
-      <div class="bg-green-500 min-h-screen">
-        <div class="bg-red-500 absolute top-0 left-0 min-h-screen">
+    <div>
+      <div>
+        <div class="bg-purp absolute top-0 left-0 min-h-screen">
           <img alt="" src='https://s3.us-west-2.amazonaws.com/henftz.xyz/tzraffles/banner.jpg' />
           <br />
           <ChangeQuantity />
