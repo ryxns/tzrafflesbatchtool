804ed7f4cde1f4a37d6d4b80ad66844315a7afbe
ryxns <33070164+ryxns@users.noreply.github.com>
2022-07-27 20:04:03 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import ChangeQuantity from "./components/ChangeName";
 
 const App = () => {
   return (
-    <div className="bg-[url('./components/banner.jpg')] ">
+    <div className="bg-[url('./components/banner.jpg')]">
       <Navbar />
-      <div className="container pt-10 mx-auto">
-        <div className="flex justify-center">
+      <div className="container pt-10 mx-auto bg-[url('./components/banner.jpg')]">
+        <div className="flex justify-center bg-[url('./components/banner.jpg')]">
           <h1 className="text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-red-500 via-blue-300 to-red-400">
             TzRaffles Batch Buy Tool
           </h1>
         </div>
-        <div className="mt-20 flex justify-center">
+        <div className="mt-20 flex justify-center bg-[url('./components/banner.jpg')]">
           <ChangeQuantity />
         </div>
 
